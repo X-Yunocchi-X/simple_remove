71dbb6d6fbb8aced7c90ede1b26cae77c5dbc5c1
X-Yunocchi-X <syf2003_bool@163.com>
2023-12-21 23:21:26 +0800

    complete error handling draft

--- src/main.zig
+++ src/main.zig
@@ -1,21 +1,24 @@
 const std = @import("std");
 
-const Remover = @import("simple_remove.zig").Remover;
+const simple_remove = @import("simple_remove.zig");
 
 const allocator = std.heap.page_allocator;
 
-pub fn main() void {
-    const args = getCommandArgs();
-    const remover = Remover.new(args) catch |err| switch (err) {
-        error.HomeNotFound => std.debug.print("Can not get environment variable HOME, use 'echo $HOME' to check", .{}),
-        else => |other_err| std.debug.print("other error occured: {}", other_err),
+pub fn main() !void {
+    const args = try getCommandArgs();
+    const remover = simple_remove.Remover.new(args) catch |err| {
+        switch (err) {
+            error.HomeNotFound => std.debug.print("Can not get environment variable HOME, use 'echo $HOME' to check\n", .{}),
+            else => |other_err| std.debug.print("other error occured: {}\n", .{other_err}),
+        }
+        return err;
     };
     _ = remover;
 
     std.process.argsFree(allocator, args);
 }
 
-fn getCommandArgs() []const [:0]u8 {
+fn getCommandArgs() ![]const [:0]u8 {
     const args = try std.process.argsAlloc(allocator);
     return args;
 }
